77d9afc2c46886b20d9d0048b73a196306c85737
Bojan Radovic <rbojan2000@gmail.com>
2024-03-31 13:36:42 +0200

    Container Orchestration

--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@ if [ "$1" == "--streams" ]; then
     if [ "$2" == "--start" ]; then
         docker network ls | grep streams_nw >/dev/null || docker network create --driver bridge streams_nw
         docker compose -f streams-insights/infrastructure/docker-compose.yml up -d
-        docker compose -f visualization/infrastructure/docker-compose.yml up -d
     elif [ "$2" == "--stop" ]; then
         docker compose -f streams-insights/infrastructure/docker-compose.yml down
-        docker compose -f visualization/infrastructure/docker-compose.yml down
     else
         echo "Invalid argument provided"
         echo "Usage: ./run.sh --streams --start/--stop"
@@ -23,9 +21,21 @@ elif [ "$1" == "--batch" ]; then
         docker compose -f air-meteo-insights/infrastructure/docker-compose.yml down
     else
         echo "Invalid argument provided"
-        echo "Usage: ./run.sh --streams --start/--stop"
+        echo "Usage: ./run.sh --batch --start/--stop"
     fi
+
+elif [ "$1" == "--visualization" ]; then
+    if [ "$2" == "--start" ]; then
+        docker network ls | grep infrastructure_nw >/dev/null || docker network create --driver bridge infrastructure_nw
+        docker compose -f visualization/infrastructure/docker-compose.yml up -d
+    elif [ "$2" == "--stop" ]; then
+        docker compose -f visualization/infrastructure/docker-compose.yml down -d
+    else
+        echo "Invalid argument provided"
+        echo "Usage: ./run.sh --visualization --start/--stop"
+    fi
+
 else
     echo "Invalid argument provided"
-    echo "Usage: ./run.sh --streams/--batch --start/--stop"
+    echo "Usage: ./run.sh --streams/--batch/--visualization --start/--stop"
 fi
